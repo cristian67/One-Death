fab1ac34a58ae2c5de9cc94962ee04703e78d8de
Cristian <cristian.rock78@gmail.com>
2019-02-06 15:09:21 -0300

    Script de Editor para identificar camino de waypoints

---
+++ Assets/One Death/Editor/AIWaypointNetworkEditor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.AI;
+
+[CustomEditor(typeof(AIWaypointNetwork))]
+public class AIWaypointNetworkEditor : Editor
+{
+
+    //Render en la vista de escena
+    void OnSceneGUI()
+    {
+        //Obtener referencias de los object con el script waypoint
+        AIWaypointNetwork network = (AIWaypointNetwork)target;
+
+        //NOMBRE DEL WAYPOINT
+        NameWaypoint(network);
+
+
+        //Colorar Conections and Path lines waypoints
+        NavegationWaypointColor(network);
+
+       
+    }
+
+
+    private void NameWaypoint(AIWaypointNetwork network)
+    {
+        //cambiar color letras
+        GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.white;
+
+        //iterar waypoint y asignacion del nombre
+        for (int i = 0; i < network._waypoints.Count; i++)
+        {
+            if (network._waypoints[i] != null)
+            {
+                Handles.Label(network._waypoints[i].position, "Waypoint " + i.ToString(), style);
+            
+            }
+        }
+    }
+
+    private void NavegationWaypointColor(AIWaypointNetwork network) {
+
+        if (network.DisplayMode == PathDisplayMode.Connections)
+        {
+
+            //Inicializar un vector vacio con la cantidad de waypoint
+            Vector3[] linePoints = new Vector3[network._waypoints.Count];
+
+            for (int i = 0; i < network._waypoints.Count; i++)
+            {
+                // index va a tomar el valor de la posicion distinta a la cantidad de waypoint o 0.
+                int index = i != network._waypoints.Count ? i : 0;
+
+                if (network._waypoints[index] != null)
+                {
+                    linePoints[i] = network._waypoints[index].position;
+                    //Debug.Log("EN FOR" + linePoints[i]);
+                }
+                else
+                {
+                    linePoints[i] = new Vector3(Mathf.Infinity, Mathf.Infinity, Mathf.Infinity);
+                    Debug.Log("EN FOR" + linePoints[i]);
+                }
+            }
+
+            //Cambiar color a la linea
+            Handles.color = Color.cyan;
+            //Metodo magico para dibujar los vectores de los waypoint
+            Handles.DrawPolyLine(linePoints);
+        }
+        else if (network.DisplayMode == PathDisplayMode.Paths)
+        {
+            NavMeshPath path = new NavMeshPath();
+            Vector3 from = network._waypoints[network.WInitial].position;
+            Vector3 to = network._waypoints[network.WEnd].position;
+
+            NavMesh.CalculatePath(from, to, NavMesh.AllAreas, path);
+
+            Handles.color = Color.yellow;
+            Handles.DrawPolyLine(path.corners);
+        }
+    }
+}
---
+++ Assets/One Death/Scripts/AIWaypointNetwork.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Enumerar opciones en el inspector 
+public enum PathDisplayMode { None, Connections, Paths }
+
+public class AIWaypointNetwork : MonoBehaviour
+{
+    //llamar a ese display en el script
+    public PathDisplayMode DisplayMode = PathDisplayMode.Connections;
+
+    //Waypoint Inicial y final 
+    public int WInitial = 0; 
+    public int WEnd = 0; 
+
+    //Lista con la transformada de los waypoints
+    public List<Transform> _waypoints = new List<Transform>();
+}
